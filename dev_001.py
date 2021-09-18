6be20a6dbdf745ff18f0d2e4f8f4df78b7b2e19f
OlivierNDO <Olivier_N@lynchburg.edu>
2021-09-18 11:45:23 -0500

    initial dev script

--- dev_001.py
+++ dev_001.py
@@ -12,6 +12,8 @@ from sklearn.preprocessing import StandardScaler, PowerTransformer, OneHotEncode
 from sklearn.compose import TransformedTargetRegressor, ColumnTransformer
 from sklearn.pipeline import FeatureUnion, Pipeline, make_pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import _VectorizerMixin
+from sklearn.feature_selection._base import SelectorMixin
 from sklearn.impute import SimpleImputer
 import random
 import time
@@ -43,52 +45,6 @@ config_x_cols = config_categ_x_cols + config_contin_x_cols
 
 
 
-class CustomTargetTransformer(BaseEstimator, TransformerMixin):
-  # no need to implement __init__ in this particular case
-  
-  def fit(self, target):
-    return self
-
-  def transform(self, target):
-    print('\n%%%%%%%%%%%%%%%custom_target_transform() called.\n')
-    target_ = target.copy() 
-    target_ = np.sqrt(target_)
-    return target_
-
-  # need to implement this too
-  def inverse_transform(self, target):
-    print('\n%%%%%%%%%%%%%%%custom_inverse_target_transform() called.\n')
-    target_ = target.copy() 
-    target_ = target_ ** 2
-    return target_
-
-
-
-
-
-def process_contin_features(self):
-    # Create binary missingness indicators and replace nan, inf values with 0
-    train_series, test_series, valid_series = [], [], []
-    cols_with_missing_vals = 0
-    for i, c in enumerate(self.contin_cols):
-        na_inf_count = count_nan_inf_values(self.train_dframe, c) + \
-        count_nan_inf_values(self.test_dframe, c) + \
-        count_nan_inf_values(self.valid_dframe, c)
-        if na_inf_count > 0:
-            print(f'Replacing {na_inf_count} NaN/Inf values in {c} and creating missingness indicator {c}_Missing')
-            train_series.append(pd.DataFrame({f'{c}_Missing' : [1 if (np.isnan(x) or np.isinf(x)) else 0 for x in self.train_dframe[c]]}))
-            test_series.append(pd.DataFrame({f'{c}_Missing' : [1 if (np.isnan(x) or np.isinf(x)) else 0 for x in self.test_dframe[c]]}))
-            valid_series.append(pd.DataFrame({f'{c}_Missing' : [1 if (np.isnan(x) or np.isinf(x)) else 0 for x in self.valid_dframe[c]]}))
-            cols_with_missing_vals += 1
-    print(f'Replaced NaN/Inf values in {cols_with_missing_vals} continuous features')
-        
-
-
-
-
-
-
-
 class MissingnessIndicatorTransformer(BaseEstimator, TransformerMixin):
   """
   Replace missing values in continuous fields with zero and create missingness
@@ -110,38 +66,76 @@ class MissingnessIndicatorTransformer(BaseEstimator, TransformerMixin):
         col_c_indicator = pd.DataFrame({f'{c}_Missing' : self.binary_missingness(iterable = target_copy[c])})
         target_missing_ind_list.append(col_c_indicator)
     target_missing_ind = pd.concat(target_missing_ind_list, axis = 1)
-    #target_missing_ind = pd.concat(pd.DataFrame({f'{c}_Missing' : [binary_missingness(target_copy[c]) for c in target_copy.columns]}), axis = 1)
-    target_copy = pd.concat([target_copy.fillna(0), target_missing_ind], axis = 1)
-    return target_copy
+    output_copy = pd.concat([target_copy.fillna(0).reset_index(drop=True), target_missing_ind.reset_index(drop=True)], axis = 1)
+    print(f'output shape: {output_copy.shape[0]})')
+    
+    return output_copy
 
 
 
 class CategoricalTransformer(BaseEstimator, TransformerMixin):
   """
-  Replace missing values in continuous fields with zero and create missingness
-  indicators in new columns. e.g. columns 'A_Missing', 'B_Missing' are
-  returned in addition to 'A' and 'B'
+  One-hot encode categorical columns in a way that will work when
+  the test set has values not found in the training set. Columns
+  returned will exclude a single reference level. e.g.
+  if a pandas.DataFrame with column 'X' and values 'A', 'B', and 'C'
+  is passed where frequencies of 'A', 'B', and 'C' and 500, 250, and 250,
+  respectively, binary columns 'X_B' and 'X_C' will be returned.
   """
-  
-  
+  def __init__(self, column_value_counts = {}):
+      self.column_value_counts = column_value_counts
       
   def fit(self, target):
-    return self
+      for column in target.columns:
+          value_count_series = target[column].value_counts()
+          key_values = list(value_count_series.index)
+          key_counts = list(value_count_series.values)
+          self.column_value_counts[column] = dict(zip(key_values, key_counts))
+      return self
 
   def transform(self, target):
-    target_copy = target.copy()
-    
-    
-    
-    return target_copy
+      target_copy = target.copy()
+      one_hot_cols = list(set(self.column_value_counts.keys()))
+      one_hot_df_list = []
+      for ohc in one_hot_cols:
+          ohc_dict = self.column_value_counts.get(ohc)
+          
+          # Remove reference level (max frequency categorical level)
+          ohc_keys = list(ohc_dict.keys())
+          ohc_values = list(ohc_dict.values())
+          use_levels = [ohc_keys for _, ohc_keys in sorted(zip(ohc_values, ohc_keys))][:-1]
+          for level in use_levels:
+              binary_values = [1 if x == level else 0 for x in target_copy[ohc]]
+              encoded_values = pd.DataFrame({f'{ohc}_{level}' : binary_values})
+              one_hot_df_list.append(encoded_values)
+      target_copy = pd.concat(one_hot_df_list, axis = 1)
+      return target_copy
+
+
+
+class LowVarianceTransformer(BaseEstimator, TransformerMixin):
+  """
+  Removes columns in numpy array with zero variance based on the training set.
+  This is necessary after missingness indicators are created for
+  every column - including columns without missing values.
+  """
+  def __init__(self, zero_variance_cols = []):
+      self.zero_variance_cols = zero_variance_cols
+      
+  def fit(self, target):
+      self.zero_variance_cols = [i for i in list(range(target.shape[1])) if len(np.unique(target[:,i])) == 1]
+      #self.zero_variance_cols = [i for c in target.columns if len(np.unique(target[c])) == 1]
+      return self
+
+  def transform(self, target):
+      target_copy = target.copy()
+      keep_cols = [c for c in list(range(target.shape[1])) if c not in self.zero_variance_cols]
+      target_copy = target_copy[:, keep_cols]
+      return target_copy
 
 
 
 
-
-
-
-sklearn.base.BaseEstimator
 
 
 ### Execute
@@ -152,53 +146,29 @@ x = df[config_x_cols]
 y = df[config_y_col]
 
 
-
-temp = MissingnessIndicatorTransformer()
-temp.fit_transform(x[['IBS_CREDIT_SCORE_NUMBER']]).shape
-temp.fit_transform(x[['ADDRESS_YEARS']]).shape
-
-
-
-
-
-numeric_transformer = Pipeline(steps=[
-    ('missingness', MissingnessIndicatorTransformer()),
-    ('imputer', SimpleImputer(strategy='median')),
-    ('scaler', StandardScaler())])
-
-categorical_transformer = OneHotEncoder(handle_unknown='ignore')
-
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numeric_transformer, config_contin_x_cols)])#,
-    #    ('cat', categorical_transformer, config_categ_x_cols)]
-    #)
+# Define Pipeline
+numeric_transformer = Pipeline(steps = [('missingness', MissingnessIndicatorTransformer()),
+                                        ('scaler', StandardScaler()),
+                                        ('zero variance column removal', LowVarianceTransformer())])
     
+
+categorical_transformer = Pipeline(steps = [('custom categorical encoder', CategoricalTransformer())])
+
+preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, config_contin_x_cols),
+                                               ('cat', categorical_transformer, config_categ_x_cols)], remainder = 'passthrough')
+    
+
     
 pipeline = Pipeline(steps = [('preprocessor', preprocessor)])
 
 
 
-#catgorical_pipeline = Pipeline()
 
-['IBS_CREDIT_SCORE_NUMBER', 'ADDRESS_YEARS', 'EMPLOYED_YEARS']
+train_x, test_x, train_y, test_y = sklearn.model_selection.train_test_split(x, y, test_size = 0.2, random_state = 912)
 
-len([1 if (np.isnan(x) or np.isinf(x)) else 0 for x in train_x['IBS_CREDIT_SCORE_NUMBER']])
-
-
-len([1 if (np.isnan(x) or np.isinf(x)) else 0 for x in train_x['ADDRESS_YEARS']])
-
-
-
-train_x, train_y, test_x, test_y = sklearn.model_selection.train_test_split(x, y, test_size = 0.2, random_state = 912)
-
-
+#pipeline.fit(train_x)
 train_x = pipeline.fit_transform(train_x)
-
-
-
-# https://www.jmlr.org/papers/volume13/bergstra12a/bergstra12a.pdf
-
+test_x = pipeline.transform(test_x)
 
 
 
@@ -207,10 +177,32 @@ train_x = pipeline.fit_transform(train_x)
 
 
 
+### TO DO
+######################################################################################################
+# transformer for response variable
+# combined transformer/pipeline object
+# start xgbtuner
 
 
 
-git clone https://github.com/OlivierNDO/xgboost_tuner.git
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
