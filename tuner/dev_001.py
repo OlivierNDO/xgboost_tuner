e4617fea5ac224726a553037adfeb68b06fec88c
OlivierNDO <Olivier_N@lynchburg.edu>
2021-09-18 15:42:35 -0500

    create configuration module

--- tuner/dev_001.py
+++ tuner/dev_001.py
@@ -23,41 +23,12 @@ import time
 import xgboost as xgb
 
 
-
-### Column Configuration
-######################################################################################################
-
-config_y_col = 'RETAINED'
-config_id_col = 'FAKE_POLICY_NUMBERS'
-config_categ_x_cols = ['PDW_PACKAGE_POLICY_FLAG', 'RATE_STATE_NAME', 'TIER_CODE',
-                        'NEW_RENEWAL_CODE', 'OCCUPATION']
-config_contin_x_cols = ['IBS_CREDIT_SCORE_NUMBER', 'ADDRESS_YEARS', 'EMPLOYED_YEARS',
-                        'POLICY_TERM_PREMIUM_AMOUNT', 'POLICY_TERM_NUMBER_OF_MONTHS',
-                        '0_3_MAJOR', '3_5_MAJOR', '0_3_MINOR', '3_5_MINOR', 'OPERATOR_AGE', 'VEH_ISO_LIAB_SYM']
-
-config_x_cols = config_categ_x_cols + config_contin_x_cols
-
-
-### File Paths
-######################################################################################################
-config_folder_path = 'D:/kemper_exercise/raw_data/'
-config_train_file_name = 'retention_sample.csv'
-
-
-
-### Filename & Directory Configuration
-######################################################################################################
-config_parent_directory = 'D:/xgboost_tuner/'
-config_model_save_dir = f'{config_parent_directory}saved_models/'
-config_pipeline_save_path = f'{config_model_save_dir}transformation_pipeline.pkl'
-
-
 ### Import Modules
 ######################################################################################################
-sys.path.append(config_parent_directory)
+parent_directory = 'D:/xgboost_tuner/'
+sys.path.append(parent_directory)
 import tuner.utils.transformation_module as trans_mod
-
-
+import tuner.utils.configuration as config
 
 
 ### Define Functions
@@ -69,11 +40,8 @@ import tuner.utils.transformation_module as trans_mod
 
 ### Execute
 ######################################################################################################
-
 # Read Data
-df = pd.read_csv(f'{config_folder_path}{config_train_file_name}')
-x = df[config_x_cols]
-y = df[config_y_col]
+df = pd.read_csv(f'{config.folder_path}{config.train_file_name}')
 
 
 # Split into Test & Train
@@ -82,9 +50,9 @@ train, test = sklearn.model_selection.train_test_split(df, test_size = 0.2, rand
 
 transformer = trans_mod.FeatureTransformer(train_df = train,
                                            test_df = test,
-                                           numeric_columns = config_contin_x_cols,
-                                           categorical_columns = config_categ_x_cols,
-                                           pipeline_save_path = config_pipeline_save_path)
+                                           numeric_columns = config.contin_x_cols,
+                                           categorical_columns = config.categ_x_cols,
+                                           pipeline_save_path = config.pipeline_save_path)
 
 
 train_x, test_x  = transformer.process_train_test_features()
---
+++ tuner/utils/configuration.py
@@ -0,0 +1,25 @@
+### Column Configuration
+######################################################################################################
+y_col = 'RETAINED'
+id_col = 'FAKE_POLICY_NUMBERS'
+categ_x_cols = ['PDW_PACKAGE_POLICY_FLAG', 'RATE_STATE_NAME', 'TIER_CODE',
+                        'NEW_RENEWAL_CODE', 'OCCUPATION']
+contin_x_cols = ['IBS_CREDIT_SCORE_NUMBER', 'ADDRESS_YEARS', 'EMPLOYED_YEARS',
+                        'POLICY_TERM_PREMIUM_AMOUNT', 'POLICY_TERM_NUMBER_OF_MONTHS',
+                        '0_3_MAJOR', '3_5_MAJOR', '0_3_MINOR', '3_5_MINOR', 'OPERATOR_AGE', 'VEH_ISO_LIAB_SYM']
+
+x_cols = categ_x_cols + contin_x_cols
+
+
+### File Paths
+######################################################################################################
+folder_path = 'D:/kemper_exercise/raw_data/'
+train_file_name = 'retention_sample.csv'
+
+
+
+### Filename & Directory Configuration
+######################################################################################################
+parent_directory = 'D:/xgboost_tuner/'
+model_save_dir = f'{parent_directory}saved_models/'
+pipeline_save_path = f'{model_save_dir}transformation_pipeline.pkl'
